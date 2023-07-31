4f03a65414dc2da49462292e5820e6124f130cb7
Huor97 <huor97ka@gmail.com>
2023-08-01 01:42:56 +0200

    v2 avoir mots dans nouvel page

---
+++ content.js
@@ -0,0 +1,23 @@
+browser.storage.local.get(["pageText"], function (result) {
+  if (result.pageText) {
+    // pour chaque sélection de mots crée un li
+    for (let i = 0; i < result.pageText.length; i++) {
+      createListItem(result.pageText[i]);
+    }
+  }
+});
+
+function createListItem(textContent) {
+  const text = document.createElement("li");
+  const btnDelet = document.createElement("button");
+  btnDelet.textContent = "X";
+  text.textContent = textContent;
+
+  // Ajoute un gestionnaire d'événements au bouton pour supprimer un élément de liste
+  btnDelet.addEventListener("click", () => {
+    text.remove();
+  });
+
+  text.appendChild(btnDelet);
+  document.getElementById("text-container").appendChild(text);
+}
